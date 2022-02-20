70a971fa2546e81183f09ff5793755b2cb163d38
jacklynlxq <jacklynlxq@gmail.com>
2022-02-20 15:00:21 +0800

    Add exception for access record not found

--- src/main/java/com/example/demo/presentation/controller/FeatureController.java
+++ src/main/java/com/example/demo/presentation/controller/FeatureController.java
@@ -1,12 +1,17 @@
 package com.example.demo.presentation.controller;
 
-import com.example.demo.presentation.dto.AccessRequest;
-import com.example.demo.presentation.dto.AccessResponse;
+import com.example.demo.domain.dto.GetAccessRequest;
+import com.example.demo.domain.dto.GetAccessResponse;
+import com.example.demo.domain.dto.UpdateAccessRequest;
 import com.example.demo.service.AccessManagementServiceImpl;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
 
 
 @Controller
@@ -16,22 +21,28 @@ public class FeatureController {
 
     @GetMapping
     @ResponseBody
-    public AccessResponse getUserFeatureAccess(@RequestParam String featureName, @RequestParam String email){
+    public GetAccessResponse getUserFeatureAccess(@RequestParam String featureName, @RequestParam String email){
         logger.info("User email: " + email);
         logger.info("Feature name: " + featureName);
         // TODO: add exception for only 1 request
         // TODO: add error message if null
         // TODO: add validation for email
 
-        AccessRequest accessRequest = new AccessRequest(email, featureName);
+        GetAccessRequest getAccessRequest = new GetAccessRequest(email, featureName);
 
         Boolean access = false;
-        if(accessManagementService.checkUserFeatureAccess(accessRequest) == null){ access = false; }
-        else{ access = true;  }
+        if(accessManagementService.checkUserFeatureAccess(getAccessRequest) == null){
+            throw new EntityNotFoundException(
+                    "Can't find record of feature access: " + featureName + " for this user email: " + email + ", please add access for the user."
+            );
+        }
+        else{
+            access = accessManagementService.checkUserFeatureAccess(getAccessRequest).getUserFeatureAccess();
+        }
 
         logger.info("Access: " + access);
 
-        return new AccessResponse(access);
+        return new GetAccessResponse(access);
     }
 
     @Autowired
@@ -39,8 +50,13 @@ public class FeatureController {
 
     @PostMapping
     @ResponseBody
-    public postUserFeatureAcess(@RequestBody ){
+    public ResponseEntity postUserFeatureAccess(@RequestBody UpdateAccessRequest updateAccessRequest){
+        // TODO: pass UpdateAccessRequest to service to repository to add into database
 
+        accessManagementService.updateUserFeatureAccess(updateAccessRequest);
+        //TODO: if not updated, return 304 not modified
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
 
